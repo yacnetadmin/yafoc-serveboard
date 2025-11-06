4b9c68fac2f2508f4e02a8181defba67b4dfc07f
fredweidemann <fweidemann@yorkalliance.org>
2025-11-06 11:48:47 -0500

    Update MSAL auth configuration and scripts

--- docs/scripts/msal-auth.js
+++ docs/scripts/msal-auth.js
@@ -3,10 +3,11 @@
 const msalConfig = {
   auth: {
     clientId: "1bad36bb-ea69-44f2-a2f5-0a23078b6715", // Fill from config
-    authority: "https://login.microsoftonline.com/7be79f78-a660-436f-a2a5-de2c1068b6db", // Fill from config
+    authority: "https://login.microsoftonline.com/common", // Using common endpoint for better compatibility
     redirectUri: window.location.origin + window.location.pathname
   }
 };
+console.log("MSAL Config:", JSON.stringify(msalConfig, null, 2));
 const msalInstance = new msal.PublicClientApplication(msalConfig);
 
 async function signIn() {
